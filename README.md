88d2df79a92d7710aba12de2bee0add60f6011fa
yosuaindra <yosua.is.dev@gmail.com>
2023-01-26 21:38:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Laravel 9 with Vite and React.js
+
+## Installation :
+
+```sh
+composer install
+npm install
+```
+
+## Running on Local :
+
+```sh
+php artisan serve
+npm run dev
+```
---
+++ resources/js/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Laravel 9 with Vite and React.js</h1>
+        </div>
+    );
+};
+
+export default App;
